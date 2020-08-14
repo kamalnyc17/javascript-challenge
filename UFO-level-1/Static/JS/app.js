51b982aca77c5917d931515865a5d066e9f14d63
Kamal Mukherjee <kamalnyc17@gmail.com>
2020-08-14 08:00:53 -0400

    plotting data & string formatting

--- UFO-level-1/Static/JS/app.js
+++ UFO-level-1/Static/JS/app.js
@@ -1,4 +1,21 @@
-// from data.js
-var tableData = data;
-console.log(data)
-// YOUR CODE HERE!
+// getting UFO data from data.js
+const tableData = data;
+
+// initializing html table as d3 object
+const tbody = d3.select("tbody");
+
+// traversing through the data arrary, formatting string and plotting on the table
+data.forEach((ufoReport) => {
+  let row = tbody.append("tr");
+  Object.entries(ufoReport).forEach(([key, value]) => {
+    let cell = row.append("td");
+    let cellValue = value
+    if ((key==="state") || (key==="country")){
+      cellValue = value.toUpperCase()
+    } else if ((key==="city") || (key==="shape")) {
+      cellValue = value.replace(/\w\S*/g, ((value) => (value.charAt(0).toUpperCase() + value.substr(1).toLowerCase())));    
+    }
+    cell.text(cellValue);
+  });
+});
+  
